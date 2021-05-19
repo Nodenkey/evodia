213b44e48249a6f3bc77d48b4030a620c8c4e563
alfredasare <alfredasare101@gmail.com>
2021-05-19 17:22:10 +0000

    Add payment

--- components/shopping-cart-components/shopping-cart-total/shoppingCartTotal.js
+++ components/shopping-cart-components/shopping-cart-total/shoppingCartTotal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from "react-redux";
+import {usePaystackPayment} from "react-paystack";
 import {Divider, Flex, Greyed, Grid, HeaderFive, Paragraph} from "../../../globals/globalStyles";
 import {PaymentButtonContainer, PriceContainer, ShoppingCartTotalWrapper} from "./shoppingCartTotalStyles";
 import Button from "../../button/button";
@@ -8,8 +9,31 @@ import {faAngleLeft} from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import {createStructuredSelector} from "reselect";
 import {selectCartTotal} from "../../../lib/redux/cart/cart.selectors";
+import {clearCart} from "../../../lib/redux/cart/cart.actions";
 
-const ShoppingCartTotal = ({total}) => {
+const ShoppingCartTotal = ({total, clearCart}) => {
+    const paymentConfig = {
+        reference: String((new Date()).getTime()),
+        email: "alfredasare101@gmail.com",
+        amount: total * 100,
+        publicKey: process.env.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY,
+        currency: "GHS"
+    };
+
+    const initializePayment = usePaystackPayment(paymentConfig);
+
+    const onSuccess = (reference) => {
+        clearCart();
+    };
+
+    const onClose = () => {
+        console.log('closed')
+    }
+
+    const handlePayment = () => {
+        initializePayment(onSuccess, onClose);
+    };
+
     return (
         <ShoppingCartTotalWrapper>
                 <PriceContainer>
@@ -20,7 +44,7 @@ const ShoppingCartTotal = ({total}) => {
                 </PriceContainer>
             <PaymentButtonContainer spaceBetween>
                <Link href='/collection/all'><Flex style={{cursor: 'pointer'}}><FontAwesomeIcon icon={faAngleLeft}/><HeaderFive style={{margin: 0}}>Continue shopping</HeaderFive></Flex></Link>
-                <div><Button type='secondary' squared fluid>Make payment</Button></div>
+                <div><Button onClick={handlePayment} type='secondary' squared fluid>Make payment</Button></div>
             </PaymentButtonContainer>
         </ShoppingCartTotalWrapper>
     );
@@ -30,4 +54,8 @@ const mapStateToProps = createStructuredSelector({
     total: selectCartTotal
 });
 
-export default connect(mapStateToProps)(ShoppingCartTotal);
+const mapDispatchToProps = dispatch => ({
+    clearCart: () => dispatch(clearCart())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShoppingCartTotal);
