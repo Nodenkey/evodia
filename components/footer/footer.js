b6543f4cf8f1d6ae2956edcf447afe3d8e68c405
nii <nodenkey@gmail.com>
2021-05-23 00:10:05 +0000

    Add footer links

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {FooterWrapper} from "./footerStyles";
 import {Container, Flex, HeaderFive} from "../../globals/globalStyles";
 import Logo from "../svg/logo/logo";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faFacebook, faInstagram, faTwitter} from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
 
 const Footer = () => {
     return (
@@ -12,14 +13,14 @@ const Footer = () => {
                 <Flex spaceBetween>
                     <Logo/>
                     <Flex>
-                        <HeaderFive>Men</HeaderFive>
-                        <HeaderFive>Women</HeaderFive>
-                        <HeaderFive>Cart</HeaderFive>
+                        <Link href='/collection/men'><HeaderFive>Men</HeaderFive></Link>
+                        <Link href='/collection/women'><HeaderFive>Women</HeaderFive></Link>
+                        <Link href='/shopping-cart'><HeaderFive>Cart</HeaderFive></Link>
                     </Flex>
                     <Flex>
-                        <FontAwesomeIcon icon={faInstagram}/>
-                        <FontAwesomeIcon icon={faFacebook}/>
-                        <FontAwesomeIcon icon={faTwitter}/>
+                        <a href="https://twitter.com/_wevops" target='_blank' rel='noopener'><FontAwesomeIcon icon={faInstagram}/></a>
+                        <a href="https://web.facebook.com/Wevops-104263934675793" target='_blank' rel='noopener'><FontAwesomeIcon icon={faFacebook}/></a>
+                        <a href="https://www.instagram.com/wevops/" target='_blank' rel='noopener'><FontAwesomeIcon icon={faTwitter}/></a>
                     </Flex>
                 </Flex>
             </Container>
