0ebc20e4b77e11b499feaa50cca66e7f5ac10e51
nii <nodenkey@gmail.com>
2021-07-08 19:49:59 +0000

    Nii test

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,19 @@
 import {GlobalStyle} from "../globals/appGlobal";
 import React from "react";
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 import Layout from "../components/layout/layout";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
-import {persistor, store} from "../lib/redux/store";
+import {store} from "../lib/redux/store";
 
 function MyApp({Component, pageProps}) {
     return (
         <Provider store={store}>
-            <PersistGate persistor={persistor}>
                 <GlobalStyle/>
                 <Layout>
                     <Component {...pageProps} />
                 </Layout>
-            </PersistGate>
         </Provider>
     );
 }
